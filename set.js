7d2a249dd28c263f8cc641df8b22ad7bd00011f8
Ken980 <kenboy9800@gmail.com>
2023-03-06 00:54:37 +0000

    remove numbers from set.txt

--- set.js
+++ set.js
@@ -2,8 +2,7 @@ const fs = require("fs");
 
 fs.readFile("set.txt", "utf-8", (err,data) => {
     if (err) throw err;
-    data = data.replace(/\n/g, "");
+    data = data.replace(/\n/g, "").replace(/[0-9]/g, "");
     console.log(data);
     console.log(typeof data);
 });
-
