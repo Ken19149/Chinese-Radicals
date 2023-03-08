1e18050fe9b7617f232f676463f6fb30586ce3cc
Ken980 <kenboy9800@gmail.com>
2023-03-08 16:32:53 +0000

    decompose set 2.txt

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 var hanzi = require("hanzi");
 hanzi.start();
 
-var decomposition = hanzi.decompose("");
-console.log(decomposition);
+const fs = require("fs");
+
+fs.readFile("set 2.txt", "utf-8", (err,data) => {
+    if (err) throw err;
+    var decomposition = hanzi.decomposeMany(data);
+    console.log(decomposition);
+});
