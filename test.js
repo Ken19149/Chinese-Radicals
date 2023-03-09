aff77e030f578924dea9eb9a3e281d531663d514
Ken980 <kenboy9800@gmail.com>
2023-03-09 11:09:28 +0000

    convert simplified -> tradtional use opencc-js module (Open Chinese Convert) to convert simplified to traditional taiwan chinese characters rename file set-2.txt to set-simplified.txt

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
+const OpenCC = require('opencc-js');
 var hanzi = require("hanzi");
 hanzi.start();
 
-const fs = require("fs");
+const converter = OpenCC.Converter({ from: 'cn', to: 'tw' });   //change simplified to taiwan traditional chinese
 
-fs.readFile("set 2.txt", "utf-8", (err,data) => {
+const fs = require("fs");   //filesystem to open file
+
+fs.readFile("set-simplified.txt", "utf-8", (err,data) => {
     if (err) throw err;
-    var decomposition = hanzi.decomposeMany(data);
+    var traditional = converter(data);         //convert simplified -> traditional
+    var decomposition = hanzi.decomposeMany(traditional);      //breakdown characters
     console.log(decomposition);
 });
