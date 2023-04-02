f1d55c6f2d4078d8f8edcec44ce37aa552081f50
Ken980 <kenboy9800@gmail.com>
2023-04-02 19:34:12 +0000

    turn radicals in the library into arrays - also read the kangxi radicals.txt with readFileSync

--- common radicals.js
+++ common radicals.js
@@ -1,6 +1,28 @@
+const fs = require("fs");
+
 var hanzi = require("hanzi");
 hanzi.lib_radicals = require("hanzi/lib/data/radicalList.js")["radicalList"];
 hanzi.lib_radicals_with_meaning = require("hanzi/lib/data/radicalListWithMeaning.js")["radicalListWithMeaning"];
+
+
+radicals_lib = hanzi.lib_radicals;
+radicals_lib_w_meaning = [];
+radicals_kangxi = [];
+
 for (let i in hanzi.lib_radicals_with_meaning) {
-    console.log(i + ": " + hanzi.lib_radicals_with_meaning[i]);
+    radicals_lib_w_meaning.push(i);
 }
+
+/*
+fs.readFile("kangxi radicals.txt", "utf-8", (err,data) => {
+    if (err) throw err;
+    for (var i in data) {
+        radicals_kangxi.push(data[i]);
+        console.log(data[i]);
+    }
+    console.log(radicals_kangxi);
+});
+*/
+
+var text = fs.readFileSync("kangxi radicals.txt", "utf8");
+console.log(text);
