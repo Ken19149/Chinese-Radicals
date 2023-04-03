9547cb81b0cc599148134fc22deb813ce8912f81
Ken980 <kenboy9800@gmail.com>
2023-04-03 22:18:38 +0000

    combine radicals in hanzi library - combine radicals and radicals with meaning

--- common radicals.js
+++ common radicals.js
@@ -5,9 +5,10 @@ hanzi.lib_radicals = require("hanzi/lib/data/radicalList.js")["radicalList"];
 hanzi.lib_radicals_with_meaning = require("hanzi/lib/data/radicalListWithMeaning.js")["radicalListWithMeaning"];
 
 
-radicals_lib = hanzi.lib_radicals;
-radicals_lib_w_meaning = [];
-radicals_kangxi = [];
+var radicals_lib = hanzi.lib_radicals;
+var radicals_lib_w_meaning = [];
+var radicals_kangxi = [];
+var radicals_combine_lib = [];
 
 for (let i in hanzi.lib_radicals_with_meaning) {
     radicals_lib_w_meaning.push(i);
@@ -17,3 +18,6 @@ var str_kangxi_radicals = fs.readFileSync("kangxi radicals.txt", "utf8");
 for (let i in str_kangxi_radicals) {
     radicals_kangxi.push(str_kangxi_radicals[i]);
 }
+
+radicals_combine_lib = radicals_lib.concat(radicals_lib_w_meaning);
+console.log(radicals_combine_lib);
