c241e602a85318411ac15e9413536afba2143587
Ken980 <kenboy9800@gmail.com>
2023-04-05 01:22:11 +0000

    remove dupes of combined radicals in library (done) from trying to remove dupes in array to change array to string then use replace function to remove the duplicates

--- common radicals.js
+++ common radicals.js
@@ -22,12 +22,18 @@ for (let i in str_kangxi_radicals) {
 //remove duplicates from combined radicals in library
 
 var radicals_combine_lib_total = radicals_lib.concat(radicals_lib_w_meaning);
+var str_radicals_combine_lib_total = "";
+var str_radicals_combine_lib_remove_dupe = "";
 
 for (let i in radicals_combine_lib_total) {
-    if (radicals_combine_lib_total != []) {
-        radicals_combine_lib = radicals_combine_lib.concat(radicals_combine_lib_total[0]);
-        
-    } else if (radicals_combine_lib_total == []) {
+    str_radicals_combine_lib_total = str_radicals_combine_lib_total.concat(radicals_combine_lib_total[i]);
+}
+
+for (let i in str_radicals_combine_lib_total) {
+    if (str_radicals_combine_lib_total != "") {
+        str_radicals_combine_lib_remove_dupe = str_radicals_combine_lib_remove_dupe.concat(str_radicals_combine_lib_total[0]);
+        str_radicals_combine_lib_total = str_radicals_combine_lib_total.split(str_radicals_combine_lib_total[0]).join("");    //replace first character with ""; remove 1st character
+    } else if (str_radicals_combine_lib_total == "") {
         break;
     }
 }
@@ -36,4 +42,4 @@ console.log("kangxi radicals: " + radicals_kangxi.length);
 console.log("radicals lib: " + radicals_lib.length);
 console.log("radicals lib with meaning:" + radicals_lib_w_meaning.length);
 console.log("combine total: " + radicals_combine_lib_total.length);
-console.log("combine remove dupe: " + radicals_combine_lib.length);
+console.log("combine remove dupe: " + str_radicals_combine_lib_remove_dupe.match(/./gu).length);
