e5dad7b4d1dcaabce1fd32b313bc7eb9713f578f
Ken980 <kenboy9800@gmail.com>
2023-04-06 22:33:29 +0000

    create function to create multple sets - the function can create n sets of array with selected interval - create the function to create sets of 1k,2k,...,5k characters - also create function called setData() to show characters between the intervals

--- main.js
+++ main.js
@@ -9,4 +9,38 @@ const fs = require("fs");   //filesystem to open file
 var set_simplified = fs.readFileSync("set-simplified.txt", "utf8")
 var traditional = converter(set_simplified);         //convert simplified -> traditional
 var decomposition = hanzi.decomposeMany(traditional);      //breakdown characters
-console.log(decomposition);
+
+//create sets of 1k - 5k characters
+interval_length = 1000
+sets_count = 5
+sets = createSets(traditional, sets_count, interval_length);    //input_set = really long string; 
+
+function createSets(input_set, sets_count = 5, interval_length = 1000){
+    var sets = []
+
+    for (let i = 0; i < sets_count; i++) {
+        sets.push([]);
+        if (i > 0) {
+            sets[i] = sets[i-1].concat(sets[i]);
+        }
+        for (let j = 0; j < interval_length;j++){
+            sets[i].push(input_set[i*interval_length+j]);
+        }
+    }
+    return sets
+}
+
+function setData(sets){
+    sets_count = sets.length;
+    interval_length = sets[0].length;
+    full_str = "";
+    str = "";
+    for (let i = 0; i < sets_count; i++) {
+        str = str.concat((((i+1)*interval_length)-1) + ": " + sets[i][((i+1)*interval_length)-2] + " " +  ((i+1)*interval_length) + ": " + sets[i][((i+1)*interval_length)-1] + " ");
+    }
+    full_str = full_str.concat("length: " + sets[sets_count-1].length, " first: " + sets[sets_count-1][0], " last: " + sets[sets_count-1][sets_count-1] + " " + str);
+    return full_str;
+}
+
+console.log(sets);
+console.log(setData(sets));
